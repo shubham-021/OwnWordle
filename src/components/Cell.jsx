4ff32587168334633442d31e47f8817b77aaf47c
shubham-021 <shubham.arka@gmail.com>
2025-07-24 13:47:37 +0530

    minor change

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -19,7 +19,7 @@ export const Cell = ({ children, correct, incorrect, includes, isFinal }) => {
           "bg-[#538d4e]": correct,
           "bg-[#2a2a2a]": incorrect,
           "bg-[#b59f3b]": includes,
-          "pop-animation": hasAnimated,
+          "pop-animation": correct || incorrect || includes,
         },
         "h-9 w-9 lg:h-13 lg:w-13 flex justify-center items-center text-neutral-200 border-2 rounded-md sm:rounded-xl border-neutral-600 uppercase"
       )}
